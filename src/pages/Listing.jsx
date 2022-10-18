607e3c1deae0b1bc510a7a8aa1be82fb6b7f8183
rifa712 <rifaalghan@gmail.com>
2022-10-18 13:01:46 +0700

    changing geoceode api

--- src/pages/Listing.jsx
+++ src/pages/Listing.jsx
@@ -27,7 +27,6 @@ const Listing = () => {
       const docSnap = await getDoc(docRef)
 
       if (docSnap.exists()) {
-        console.log(docSnap.data())
         setListing(docSnap.data())
         setLoading(false)
       }
